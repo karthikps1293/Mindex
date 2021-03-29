4a02f14e75f11e737f644125f3365ee0609ff21d
karthikps1293 <ks8536@rit.edu>
2021-03-28 22:34:10 -0400

    Added failure test cases for Task 2

--- src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
+++ src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.UUID;
@@ -41,6 +42,7 @@ public class CompensationServiceImplTest {
     @Test
     public void testCreateRead() {
         Compensation testCompensation = new Compensation();
+        // create compensation using valid employeeId
         testCompensation.setEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
         testCompensation.setSalary(80000);
         testCompensation.setEffectiveDate("08/25/2018");
@@ -59,6 +61,32 @@ public class CompensationServiceImplTest {
         assertNotNull(readCompensation);
         assertCompensationEquivalence(createdCompensation, readCompensation);
 
+    }
+
+    @Test
+    public void testCreateInvalidEmployeeId(){
+        Compensation testCompensation = new Compensation();
+        // create compensation using invalid employeeId
+        testCompensation.setEmployeeId(UUID.randomUUID().toString());
+        testCompensation.setSalary(70000);
+        testCompensation.setEffectiveDate("08/25/2018");
+
+        // confirm that compensation creation fails as expected
+        ResponseEntity<Compensation> responseEntity = restTemplate.postForEntity(compensationUrl, testCompensation,
+                Compensation.class);
+        assertEquals(responseEntity.getStatusCodeValue(), 500);
+    }
+
+    @Test
+    public void testCreateNullEmployeeId(){
+        Compensation testCompensation = new Compensation();
+        testCompensation.setSalary(70000);
+        testCompensation.setEffectiveDate("08/25/2018");
+
+        // confirm that compensation creation fails as expected
+        ResponseEntity<Compensation> responseEntity = restTemplate.postForEntity(compensationUrl, testCompensation,
+                Compensation.class);
+        assertEquals(responseEntity.getStatusCodeValue(), 500);
     }
 
     private static void assertCompensationEquivalence(Compensation expected, Compensation actual) {
